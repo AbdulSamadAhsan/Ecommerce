70d5c73d79f659a3a6672c816910ee77ac9ab054
Syed Abdul Samad Ahsan <78288727+Abdul711@users.noreply.github.com>
2026-06-18 03:11:25 +0500

    Add customer sidebar

---
+++ resources/views/livewire/pages/frontend/customer/sidebar.blade.php
@@ -0,0 +1,6 @@
+<div class="list-group shadow-sm rounded-4 overflow-hidden">
+<a wire:navigate href="{{ route('customer.orders') }}" class="list-group-item">Orders</a>
+<a wire:navigate href="{{ route('customer.returns') }}" class="list-group-item">Returns</a>
+<a wire:navigate href="{{ route('customer.wallet') }}" class="list-group-item">Wallet</a>
+<a wire:navigate href="{{ route('customer.profile') }}" class="list-group-item">Profile</a>
+</div>
